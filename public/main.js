40951a64db6e3367f0fc636e1b6f6e5c419764c8
Susan Gooding <susan.gooding@yahoo.co.uk>
2017-04-16 08:42:18 +0100

    basic

--- public/main.js
+++ public/main.js
@@ -13,6 +13,8 @@ $(function(){
       // We need an absolute path here because its been called from the client side
       url: 'http://localhost:3333/cars',
     }).done(function(cars){
+
+
         // Loop through the response which we have called cars
         cars.forEach(function(car){
           // Use Handlebars template and insert data for that car in loop
@@ -20,6 +22,8 @@ $(function(){
           // Get the cars list we defined above and append the Handlebars template
           $carsList.append(newHTML);
         });
+
+
       }).fail(function(err){ //just to show you how to error catch
         console.log('Error', err.message);
       });

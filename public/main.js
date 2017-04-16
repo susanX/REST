71ab3a8c42237742c58d50bb2c7dde119c1a5fcf
Susan Gooding <susan.gooding@yahoo.co.uk>
2017-04-16 02:25:10 +0100

    basic server

---
+++ public/main.js
@@ -0,0 +1,26 @@
+// This is your client js code
+// This is where we do our request from and manipulate the html page
+$(function(){
+
+  // Using jQuery we are access elements and assigning the Handlebars template
+  var $carsList = $('#cars-list');//
+  var source = $('#car-template').html();//a
+  var templateFn = Handlebars.compile(source);//b
+
+  // GET request to the Server 'server.js'
+  $.ajax({
+      method: 'GET',
+      // We need an absolute path here because its been called from the client side
+      url: 'http://localhost:3333/cars',
+    }).done(function(cars){
+        // Loop through the response which we have called cars
+        cars.forEach(function(car){
+          // Use Handlebars template and insert data for that car in loop
+          var newHTML = templateFn(car);
+          // Get the cars list we defined above and append the Handlebars template
+          $carsList.append(newHTML);
+        });
+      }).fail(function(err){ //just to show you how to error catch
+        console.log('Error', err.message);
+      });
+})
--- server.js
+++ server.js
@@ -1,7 +1,48 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(express.static('public'));
+//db
+var mongoose = require('mongoose'); // DB control program
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/ga-server-demo');
+var asset = require('assert');//sf
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('DATABASE CONNECTED!!!!!');
+});
+var cars = db.collection('cars'); //sf
+//var cursor = cars.find({name: "Bentley"}.limit(1));
+//console.log(cursor);
 
-app.use(express.static('public/index.html'));
+//db
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// You can start to work on your rest calls now here Creat Read Update and Delete
+// Its currently hard hard but you should be able to query the database and do the
+// same thing with the results
+app.get('/cars', function(req, res){
+    var cars = [{name: 'Mercedes', bhp: 100}, {name: 'Bentley', bhp:100}];
+    return res.json(cars);
+});
+////-----
+
+
+
+
+///
+app.post('/cars', function(request, response){
+    return res.json(req.body);
+});
 
 app.listen(3333, function(){
     console.log('Server is listening');
